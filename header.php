3f48914703d41312084e4ca37f454148b8c8af05
Jarkko Saltiola <jarkko@saltiolacode.fi>
2024-12-01 12:52:14 +0200

    Add base-layout using Phel HTML library

--- header.php
+++ header.php
@@ -1,11 +1,1 @@
-<!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-
-	<?php wp_head(); ?>
-</head>
-<!-- TODO create layout with phel/html -->
 <!-- Dummy header.php file to avoid deprecation warning about it missing. -->
